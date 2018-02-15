a68ecea809a19e78677c765208253c8105716d1a
Dorfinger Jonas <it170133@EDU.HTL-LEONDING.AC.AT>
2018-02-15 10:17:23 +0100

    test to output rename

--- scripts/java.js
+++ scripts/java.js
@@ -13,7 +13,7 @@ var currentOperation = "";
 var accumulator = 0;
 
 function equal(){
-	test.innerHTML = "";
+	output.innerHTML = "";
 	accumulator = 0;
 	for(var i = 0; i < calculationNumbers.length; i++){
 		if('0' <= calculationNumbers[i] && calculationNumbers[i] <= '9'){
@@ -62,82 +62,82 @@ function reset() {
 	calculationNumbers = "^";
 	accumulator = 0;
 	currentNumber = 0;
-	test.innerHTML = calculationNumbers;
+	output.innerHTML = calculationNumbers;
 	Ergebnis.innerHTML = "";
 }
 
 function plus() {
 	calculationNumbers = calculationNumbers + "+";
-	test.innerHTML = calculationNumbers;
+	output.innerHTML = calculationNumbers;
 }
 
 function minus(){
 	calculationNumbers = calculationNumbers + "-";
-	test.innerHTML = calculationNumbers;
+	output.innerHTML = calculationNumbers;
 }
 
 function multiply() {
 	calculationNumbers = calculationNumbers + "*";
-	test.innerHTML = calculationNumbers;
+	output.innerHTML = calculationNumbers;
 }
 
 function divide() {
 	calculationNumbers = calculationNumbers + "/";
-	test.innerHTML = calculationNumbers;
+	output.innerHTML = calculationNumbers;
 }
 
 function numblock7() {
 	calculationNumbers = calculationNumbers + "7";
-	test.innerHTML = calculationNumbers;
+	output.innerHTML = calculationNumbers;
 }
 
 function numblock8() {
 	calculationNumbers = calculationNumbers + "8";
-	test.innerHTML = calculationNumbers;
+	output.innerHTML = calculationNumbers;
 }
 
 function numblock9() {
 	calculationNumbers = calculationNumbers + "9";
-	test.innerHTML = calculationNumbers;
+	output.innerHTML = calculationNumbers;
 }
 
 
 function numblock4() {
 	calculationNumbers = calculationNumbers + "4";
-	test.innerHTML = calculationNumbers;
+	output.innerHTML = calculationNumbers;
 }
 
 
 function numblock5() {
 	calculationNumbers = calculationNumbers + "5";
-	test.innerHTML = calculationNumbers;
+	output.innerHTML = calculationNumbers;
 }
 
 
 function numblock6() {
 	calculationNumbers = calculationNumbers + "6";
-	test.innerHTML = calculationNumbers;
+	output.innerHTML = calculationNumbers;
 }
 
 
 function numblock1() {
 	calculationNumbers = calculationNumbers + "1";
-	test.innerHTML = calculationNumbers;
+	output.innerHTML = calculationNumbers;
 }
 
 
 function numblock2() {
 	calculationNumbers = calculationNumbers + "2";
-	test.innerHTML = calculationNumbers;
+	output.innerHTML = calculationNumbers;
 }
 
 
 function numblock3() {
 	calculationNumbers = calculationNumbers + "3";
-	test.innerHTML = calculationNumbers;
+	output.innerHTML = calculationNumbers;
 }
 
 function numblock0() {
 	calculationNumbers = calculationNumbers + "0";
-	test.innerHTML = calculationNumbers;
+	output.innerHTML = calculationNumbers;
 }
