ab24ca8f1888a9c3d2ca0b005b98d31d9ca6a1b2
QuirinEcker <quirin.ecker@bajupa.com>
2018-02-28 17:29:50 +0100

    Creating another js file and a new html file for the upcoming designs

---
+++ scripts/Calculator.js
@@ -0,0 +1,60 @@
+"use strict";
+
+var calculationNumbers = "^" + calculationNumbers;
+var calculationEquals = 0;
+var currentDigit = 0;
+var currentDigitSave = 0;
+var currentNumber = 0;
+var currentOperation = "";
+var accumulator = 0;
+
+function equal(){
+	output.innerHTML = "";
+	accumulator = 0;
+	for(var i = 0; i < calculationNumbers.length; i++){
+		if('0' <= calculationNumbers[i] && calculationNumbers[i] <= '9'){
+			currentDigit = calculationNumbers[i] - '0';
+			currentNumber = currentNumber * 10 + currentDigit;
+		} else if (calculationNumbers[i] === "+" || calculationNumbers[i] === "-"|| calculationNumbers[i] === "/" || calculationNumbers[i] === "*") {
+			calculate(calculationNumbers[i]);
+			currentNumber = 0;
+		}
+	}
+	calculate('');
+	currentNumber = 0;
+	Ergebnis.innerHTML = accumulator;
+}
+
+function calculate(operation) {
+		if(currentOperation !== ""){
+		switch(currentOperation){
+			case ",":
+				currentDigitSave = currentDigit;
+				break;
+			case "+":
+				accumulator += currentNumber;
+				break;
+			case "-":
+				accumulator -= currentNumber;
+				break;
+			case "*":
+				accumulator *= currentNumber;
+				break;
+			case "/":
+				accumulator /= currentNumber;
+				break;
+	   }
+	} else {
+		accumulator = currentNumber;
+	}
+	currentOperation = operation;
+}
+
+function reset() {
+	calculationEquals = 0;
+	calculationNumbers = "^";
+	accumulator = 0;
+	currentNumber = 0;
+	output.innerHTML = calculationNumbers;
+	Ergebnis.innerHTML = "";
+}
--- scripts/numblock.js
+++ scripts/numblock.js
@@ -1,70 +1,7 @@
 "use strict";
 
-var calculationNumbers = "^";
-var calculationEquals = 0;
-var components = [];
-var component = 0;
-var operator = "";
+var calculationNumber = "^"
 
-var currentDigit = 0;
-var currentDigitSave = 0;
-var currentNumber = 0;
-var currentOperation = "";
-var accumulator = 0;
-
-function equal(){
-	output.innerHTML = "";
-	accumulator = 0;
-	for(var i = 0; i < calculationNumbers.length; i++){
-		if('0' <= calculationNumbers[i] && calculationNumbers[i] <= '9'){
-			currentDigit = calculationNumbers[i] - '0';
-			currentNumber = currentNumber * 10 + currentDigit;
-		} else if (calculationNumbers[i] === "+" || calculationNumbers[i] === "-"|| calculationNumbers[i] === "/" || calculationNumbers[i] === "*") {
-			calculate(calculationNumbers[i]);
-			currentNumber = 0;
-		}
-	}
-	calculate('');
-	currentNumber = 0;
-	Ergebnis.innerHTML = accumulator;
-}
-
-function calculate(operation) {
-		if(currentOperation !== ""){
-		switch(currentOperation){
-			case ",":
-				currentDigitSave = currentDigit;
-				break;
-			case ("+" || "-" || "/" || "*"):
-				accumulator *= 10 ^ currentDigitSave;
-				break;
-			case "+":
-				accumulator += currentNumber;
-				break;
-			case "-":
-				accumulator -= currentNumber;
-				break;
-			case "*":
-				accumulator *= currentNumber;
-				break;
-			case "/":
-				accumulator /= currentNumber;
-				break;
-	   }
-	} else {
-		accumulator = currentNumber;
-	}
-	currentOperation = operation;
-}
-
-function reset() {
-	calculationEquals = 0;
-	calculationNumbers = "^";
-	accumulator = 0;
-	currentNumber = 0;
-	output.innerHTML = calculationNumbers;
-	Ergebnis.innerHTML = "";
-}
 
 function plus() {
 	calculationNumbers = calculationNumbers + "+";
@@ -78,6 +15,11 @@ function minus(){
 
 function multiply() {
 	calculationNumbers = calculationNumbers + "*";
+	output.innerHTML = calculationNumbers;
+}
+
+function comma() {
+	calculationNumbers = calculationNumbers + ",";
 	output.innerHTML = calculationNumbers;
 }
 
