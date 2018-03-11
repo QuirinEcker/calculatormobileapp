171c1f87db20a4a28be5ececa287ae2daeb708b3
QuirinEcker <quirin.ecker@bajupa.com>
2018-03-11 04:42:43 +0100

    Work in Progress sedemenu

---
+++ scripts/SideMenu.js
@@ -0,0 +1,8 @@
+function toggleSideMenu() {
+	var sidemenu = document.getElementById("sideMenuContent");
+    if (sidemenu.style.display === "none") {
+        sidemenu.style.display = "block";
+    } else {
+        sidemenu.style.display = "none";
+    }
+}
